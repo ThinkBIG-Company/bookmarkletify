cfa6c6a9b6cbac60ae3d9059c0fddfe846fc8293
ThinkBIG-Company <developers@think-big-company.com>
2021-06-16 23:42:13 +0200

    ES6 Support!!

--- bookmarkletify.js
+++ bookmarkletify.js
@@ -1,13 +1,14 @@
-var jsp = require("uglify-js").parser;
-var pro = require("uglify-js").uglify;
+var UglifyJS = require("uglify-js");
 
-module.exports = function(source){
-  var min = encodeURI(minify(source));
-  var result = 'javascript:(function(){;' + min + ';})()';
-  return result;
+module.exports = function(source) {
+    var min = encodeURI(minify(source));
+    var result = 'javascript:(function(){;' + min + ';})()';
+    return result;
 }
 
-function minify(source){
-  var ast = jsp.parse(source); 
-  return pro.gen_code(ast);
+function minify(source) {
+    var result = UglifyJS.minify(source);
+	//console.log(result.error); // runtime error, or `undefined` if no error
+	//console.log(result.code);  // minified output: function add(n,d){return n+d}
+    return result.code;
 }
