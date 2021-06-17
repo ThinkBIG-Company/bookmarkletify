065c095eea076e3e8f64a125cf6e28449aaa12cf
ThinkBIG-Company <developers@think-big-company.com>
2021-06-17 14:45:22 +0200

    Update bookmarkletify.js

--- bookmarkletify.js
+++ bookmarkletify.js
@@ -1,14 +1,22 @@
 var UglifyJS = require("uglify-js");
 
-module.exports = function(source) {
-    var min = encodeURI(minify(source));
+module.exports = function(code) {
+    var min = encodeURI(minify(code));
     var result = 'javascript:(function(){;' + min + '})()';
     return result;
 }
 
-function minify(source) {
-    var result = UglifyJS.minify(source);
-	//console.log(result.error); // runtime error, or `undefined` if no error
-	//console.log(result.code);  // minified output: function add(n,d){return n+d}
+function minify(code) {
+    var result = UglifyJS.minify(code, {
+        parse: {},
+        compress: false,
+        mangle: false,
+        output: {
+            ast: false,
+            code: false // optional - faster if false
+        }
+    });
+    //console.log(result.error); // runtime error, or `undefined` if no error
+    //console.log(result.code);  // minified output: function add(n,d){return n+d}
     return result.code;
 }
